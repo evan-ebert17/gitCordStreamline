fc2301b189363d747470e51cb40411a9783e47e8
Dave Strider <evan.ebert17@gmail.com>
2024-03-31 19:56:40 -0500

    adding a dynamic filereading system for all commands in the commands/utility/ directory

--- bot.js
+++ bot.js
@@ -1,6 +1,46 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+//filesystem used to read commands
+const fs = require('node:fs');
+
+//path
+const path = require('node:path');
+
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config()
+
+//extends the map class and inclused more useful (for discord) functionality.
+client.commands = new Collection();
+
+//constructs a path to the commands directory
+const foldersPath = path.join(__dirname, 'commands');
+
+//reads the path to the directory and returns an array of all the folder names it contains.
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+
+    //constructs a path to the commands directory, another layer deep
+	const commandsPath = path.join(foldersPath, folder);
+
+    //reads the path to the directory and returns an array of all the files it contains.
+    //applies a filter on to the files to make sure only the javascript files get read.
+
+    //this reads as:
+
+    //dynamically set each commands to the clinet.commands Collection.
+    //for each file being loaded:
+    //check that it has at least a data and execute property (this helps to prevent errors resulting from loading unfinished commands.)
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
 
 // Create a new client instance
 //the guilds intent is neccesary to cache: guilds (servers), channels and roles.

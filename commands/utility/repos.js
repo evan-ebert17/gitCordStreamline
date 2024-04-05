23d47a603f538eb09a402ede8c3f04316fef0d82
Dave Strider <evan.ebert17@gmail.com>
2024-04-05 01:34:56 -0500

    changed ping.js to repos.js, repos.js now contains the groundwork for all the repository related bot functions (currently working)

---
+++ commands/utility/repos.js
@@ -0,0 +1,111 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+//module.exports is what we use in node to export data to be "require()"-d in other files.
+
+const {Octokit, App} = require("octokit");
+
+require('dotenv').config()
+
+//creating an octokit instance
+
+const octokit = new Octokit({
+	auth: process.env.OCTOKIT_AUTH
+})
+
+module.exports = {
+
+    //an example slash command.
+    //the minimum requirements for a slash command -> a name and a description
+    //below it, we define an async method (named execute) and we await the ping (unrelated to the name 'ping')
+    //the most basic application of this is interaction.reply() which will just send X message back.
+
+    //data: provides the command definiton 
+    //execute: contain the functionality to run from our event handler when the command is used.
+    cooldown: 5,
+	data: new SlashCommandBuilder()
+		.setName('repos')
+		.setDescription('Gets information on repos of the specified GitHub account.')
+
+        //subcommands are just branching commands that require different options depending on the subcommand chosen
+        //this subcommand gets the username from a user and gets all of the (public) repos associated with that username
+        .addSubcommand(subcommand =>
+            subcommand
+                    //get repos is the name of one of the subcommands, and is what we'll use to call in the execute block
+                    .setName('getallrepos')
+                    .setDescription('Gets all repos of user whose repos we want.')
+                    
+                    //this specifies the information we're asking from the user, and we added .setRequired to say "you have to put this".
+                    .addStringOption(option => 
+                        option.setName('username')
+                        .setDescription('The GitHub username of the user whose repos we want.')
+                        .setRequired(true))
+
+                    //this is just a thing asking "hey do you want this private or not".
+                    .addBooleanOption(option =>
+                        option.setName('private')
+                        .setDescription('Whether or not this command should be for your eyes only'))
+                )
+        .addSubcommand(subcommand =>
+            subcommand
+                    //gets repo of specific name
+                    .setName('getspecificrepo')
+                    .setDescription('Gets a specific repo from a specific user.')
+
+                    .addStringOption(option =>
+                        option.setName('username')
+                        .setDescription('The GitHub username of the user whose repo we want.')
+                        .setRequired(true))
+                    
+                    .addStringOption(option => 
+                        option.setName('repository')
+                        .setDescription('The name of the repository we want.')
+                        .setRequired(true))),
+
+	    async execute(interaction) {
+            //if we have subcommands, we need to set this "CommandInteractionOptionResolver#getSubcommand()" to tell us which subcommand was used.
+		    if (interaction.options.getSubcommand() === 'getallrepos') {
+
+                const username = interaction.options.getString('username');
+
+                // await interaction.reply(`Username is: ${username}`);
+
+                await octokit.request(`GET /users/${username}/repos`, {
+                    //username: username,
+                    headers: {
+                        'X-Github-Api-Version': '2022-11-28'
+                    }
+                })
+            }
+
+            if (interaction.options.getSubcommand() === 'getspecificrepo') {
+
+                const username = interaction.options.getString('username');
+
+                const repoName = interaction.options.getString('repository');
+
+                const response = await octokit.request(`GET /repos/${username}/${repoName}`, {
+                    owner: username,
+                    repo: repoName,
+                    headers: {
+                      'X-GitHub-Api-Version': '2022-11-28'
+                    }
+                  })
+
+
+                const repoInfo = 
+                `
+                Repository Name: ${response.data.name}
+                Description: ${response.data.description}
+                Language: ${response.data.language}
+                Stars: ${response.data.stargazers_count}
+                Watchers: ${response.data.watchers_count}
+                Forks: ${response.data.forks_count}
+                URL: ${response.data.html_url}
+                `;
+
+                await interaction.reply(repoInfo);
+
+            }
+
+	},
+};
